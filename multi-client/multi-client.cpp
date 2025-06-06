5754288a36b64b4a6af9c33daf224cd70b545cad
tramntb1633 <tramntb1633@gmail.com>
2025-06-06 08:13:55 +0700

    moi

--- multi-client/multi-client.cpp
+++ multi-client/multi-client.cpp
@@ -7,10 +7,3 @@
 #define PORT 2808 
 using namespace std;
 
-int generateReward() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 5);
-    int rewards[] = {100, 200, 300, 0, 50, 150};
-    return rewards[dis(gen)];
-}
